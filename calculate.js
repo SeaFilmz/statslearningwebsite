71f206fdb8b052db4803656c13cb5bf14f7dc749
Daniel Schmidt <danielschmidt18@gmail.com>
2022-09-18 11:46:06 -0700

    1st Commit of Stats Website
    This is a website that has 2 number input fields, a drop down with 4 math operators and a calculation button. That button runs math on 2 numbers based on the math operator you select.

---
+++ calculate.js
@@ -0,0 +1,20 @@
+document
+  .querySelector("#calcButton")
+  .addEventListener("click", function calcNumbers() {
+    const number1 = document.querySelector("#num1").value;
+    const number2 = document.querySelector("#num2").value;
+
+    const mathOperator = document.querySelector("#mathOperations").value;
+
+    const calculateAnswerNumber = document.querySelector("#calcAnswer");
+
+    if (mathOperator === "add") {
+      calculateAnswerNumber.innerHTML = +number1 + +number2;
+    } else if (mathOperator === "subtract") {
+      calculateAnswerNumber.innerHTML = +number1 - +number2;
+    } else if (mathOperator === "multiple") {
+      calculateAnswerNumber.innerHTML = +number1 * +number2;
+    } else if (mathOperator === "divide") {
+      calculateAnswerNumber.innerHTML = +number1 / +number2;
+    }
+  });
