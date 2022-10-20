be8b34db2e2f8dfbe7f9528f41fa114f0993685c
Daniel Schmidt <danielschmidt18@gmail.com>
2022-10-19 21:12:19 -0700

    Move Hint Text to questions Array of Objects
    Move hint text to questions array of objects to better organize my code.

--- calculate.js
+++ calculate.js
@@ -33,12 +33,12 @@ function displayHintText(docSelector, hintHelp) {
 
 //prettier-ignore
 const questions = [
-  { answerButton: selector("#maxButton"),inputField: selector("#maxInput"), correctAnswer: 9, answerDisplay: selector("#maxAnswer") },
-  { answerButton: selector("#minButton"), inputField: selector("#minInput"), correctAnswer: 2, answerDisplay: selector("#minAnswer") },
-  { answerButton: selector("#rangeButton"), inputField: selector("#rangeInput"), correctAnswer: 9-2, answerDisplay: selector("#rangeAnswer") },
-  { answerButton: selector("#sumButton"), inputField: selector("#sumInput"), correctAnswer: 3+9+6+2, answerDisplay: selector("#sumAnswer") },
-  { answerButton: selector("#avgButton"), inputField: selector("#avgInput"), correctAnswer: 20/4, answerDisplay: selector("#avgAnswer") },
-  { answerButton: selector("#medianButton"), inputField: selector("#medianInput"), correctAnswer: (6+3)/2, answerDisplay: selector("#medianAnswer") }
+  { answerButton: selector("#maxButton"),inputField: selector("#maxInput"), correctAnswer: 9, answerDisplay: selector("#maxAnswer"), hintText: "Maximum of a series of numbers is the largest number in that series" },
+  { answerButton: selector("#minButton"), inputField: selector("#minInput"), correctAnswer: 2, answerDisplay: selector("#minAnswer"), hintText: "Minimum of a series of numbers is the smallest number in that series" },
+  { answerButton: selector("#rangeButton"), inputField: selector("#rangeInput"), correctAnswer: 9-2, answerDisplay: selector("#rangeAnswer"), hintText: "Range is the maximum number minus the minimum number of a series of numbers" },
+  { answerButton: selector("#sumButton"), inputField: selector("#sumInput"), correctAnswer: 3+9+6+2, answerDisplay: selector("#sumAnswer"), hintText:  "Sum is the total of every number in the series of numbers added together"},
+  { answerButton: selector("#avgButton"), inputField: selector("#avgInput"), correctAnswer: 20/4, answerDisplay: selector("#avgAnswer"), hintText: "Average is the sum divided by the number of numbers in the series of numbers" },
+  { answerButton: selector("#medianButton"), inputField: selector("#medianInput"), correctAnswer: (6+3)/2, answerDisplay: selector("#medianAnswer"), hintText: "Median is the middle number of a series of numbers sorted from minimum to maximum. If series of numbers is even take the 2 most middle numbers, add them together and divide them by 2." }
 ];
 
 //prettier-ignore
@@ -49,45 +49,27 @@ for(let i = 0; i < 6; i++) {
 }
 
 selector("#maxHintButton").addEventListener("click", function maxHint() {
-  displayHintText(
-    questions[0].answerDisplay,
-    "Maximum of a series of numbers is the largest number in that series"
-  );
+  displayHintText(questions[0].answerDisplay, questions[0].hintText);
 });
 
 selector("#minHintButton").addEventListener("click", function minHint() {
-  displayHintText(
-    questions[1].answerDisplay,
-    "Minimum of a series of numbers is the smallest number in that series"
-  );
+  displayHintText(questions[1].answerDisplay, questions[1].hintText);
 });
 
 selector("#rangeHintButton").addEventListener("click", function rangeHint() {
-  displayHintText(
-    questions[2].inputField.value,
-    "Range is the maximum number minus the minimum number of a series of numbers"
-  );
+  displayHintText(questions[2].answerDisplay, questions[2].hintText);
 });
 
 selector("#sumHintButton").addEventListener("click", function sumHint() {
-  displayHintText(
-    questions[3].answerDisplay,
-    "Sum is the total of every number in the series of numbers added together"
-  );
+  displayHintText(questions[3].answerDisplay, questions[3].hintText);
 });
 
 selector("#avgHintButton").addEventListener("click", function avgHint() {
-  displayHintText(
-    questions[4].answerDisplay,
-    "Average is the sum divided by the number of numbers in the series of numbers"
-  );
+  displayHintText(questions[4].answerDisplay, questions[4].hintText);
 });
 
 selector("#medianHintButton").addEventListener("click", function medianHint() {
-  displayHintText(
-    questions[5].answerDisplay,
-    "Median is the middle number of a series of numbers sorted from minimum to maximum. If series of numbers is even take the 2 most middle numbers, add them together and divide them by 2."
-  );
+  displayHintText(questions[5].answerDisplay, questions[5].hintText);
 });
 
 selector("#calculateScoreButton").addEventListener(
